1474ce78fc2402a59e9f00f4249e775b6831eaab
Muhammad Arsalan <arsalanirfan888@gmail.com>
2018-09-02 15:10:42 +0500

    add basic functionality

--- src/app.js
+++ src/app.js
@@ -7,77 +7,22 @@ let recordedChunks = []
 let numRecordedChunks = 0
 let recorder
 let includeMic = false
-// let includeSysAudio = false
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#record-desktop').addEventListener('click', recordDesktop)
-  document.querySelector('#record-camera').addEventListener('click', recordCamera)
-  document.querySelector('#record-window').addEventListener('click', recordWindow)
-  document.querySelector('#play-video').addEventListener('click', playVideo)
-  document.querySelector('#micro-audio').addEventListener('click', microAudioCheck)
-  // document.querySelector('#system-audio').addEventListener('click', sysAudioCheck)
   document.querySelector('#record-stop').addEventListener('click', stopRecording)
-  document.querySelector('#play-button').addEventListener('click', play)
-  document.querySelector('#download-button').addEventListener('click', download)
 })
-
-const playVideo = () => {
-  remote.dialog.showOpenDialog({properties: ['openFile']}, (filename) => {
-    console.log(filename)
-    let video = document.querySelector('video')
-    video.muted = false
-    video.src = filename
-  })
-}
 
 const disableButtons = () => {
   document.querySelector('#record-desktop').disabled = true
-  document.querySelector('#record-camera').disabled = true
-  document.querySelector('#record-window').disabled = true
   document.querySelector('#record-stop').hidden = false
-  document.querySelector('#play-button').hidden = true
-  document.querySelector('#download-button').hidden = true
 }
 
 const enableButtons = () => {
   document.querySelector('#record-desktop').disabled = false
-  document.querySelector('#record-camera').disabled = false
-  document.querySelector('#record-window').disabled = false
   document.querySelector('#record-stop').hidden = true
-  document.querySelector('#play-button').hidden = true
-  document.querySelector('#download-button').hidden = true
 }
 
-const microAudioCheck = () => {
-  // includeSysAudio = false
-  // document.querySelector('#system-audio').checked = false
-
-  // Mute video so we don't play loopback audio.
-  var video = document.querySelector('video')
-  video.muted = true
-  includeMic = !includeMic
-  if(includeMic)
-    document.querySelector('#micro-audio-btn').classList.add('active');
-  else
-    document.querySelector('#micro-audio-btn').classList.remove('active');
-  console.log('Audio =', includeMic)
-
-  if (includeMic) {
-    navigator.webkitGetUserMedia({ audio: true, video: false },
-        getMicroAudio, getUserMediaError)
-  }
-}
-
-// function sysAudioCheck () {
-  // // Mute video so we don't play loopback audio
-  // var video = document.querySelector('video')
-  // video.muted = true
-
-  // includeSysAudio = !includeSysAudio
-  // includeMic = false
-  // document.querySelector('#micro-audio').checked = false
-  // console.log('System Audio =', includeSysAudio)
-// };
 
 const cleanRecord = () => {
   let video = document.querySelector('video');
@@ -85,30 +30,19 @@ const cleanRecord = () => {
   recordedChunks = []
   numRecordedChunks = 0
 }
-
+ 
 ipcRenderer.on('source-id-selected', (event, sourceId) => {
   // Users have cancel the picker dialog.
   if (!sourceId) return
   console.log(sourceId)
+  document.querySelector('#view-1').classList.remove('hidden')
+  document.querySelector('#view-2').classList.add('hidden')
   onAccessApproved(sourceId)
 })
 
 const recordDesktop = () => {
   cleanRecord()
   ipcRenderer.send('show-picker', { types: ['screen'] })
-}
-
-const recordWindow = () => {
-  cleanRecord()
-  ipcRenderer.send('show-picker', { types: ['window'] })
-}
-
-const recordCamera = () => {
-  cleanRecord()
-  navigator.webkitGetUserMedia({
-    audio: false,
-    video: { mandatory: { minWidth: 1280, minHeight: 720 } }
-  }, getMediaStream, getUserMediaError)
 }
 
 const recorderOnDataAvailable = (event) => {
@@ -121,35 +55,12 @@ const recorderOnDataAvailable = (event) => {
 const stopRecording = () => {
   console.log('Stopping record and starting download')
   enableButtons()
-  document.querySelector('#play-button').hidden = false
-  document.querySelector('#download-button').hidden = false
   recorder.stop()
+  document.querySelector('#view-1').classList.add('hidden')
+  document.querySelector('#view-2').classList.remove('hidden')
   localStream.getVideoTracks()[0].stop()
 }
 
-const play = () => {
-  // Unmute video.
-  let video = document.querySelector('video')
-  video.controls = true;
-  video.muted = false
-  let blob = new Blob(recordedChunks, {type: 'video/webm'})
-  video.src = window.URL.createObjectURL(blob)
-}
-
-const download = () => {
-  let blob = new Blob(recordedChunks, {type: 'video/webm'})
-  let url = URL.createObjectURL(blob)
-  let a = document.createElement('a')
-  document.body.appendChild(a)
-  a.style = 'display: none'
-  a.href = url
-  a.download = 'electron-screen-recorder.webm'
-  a.click()
-  setTimeout(function () {
-    document.body.removeChild(a)
-    window.URL.revokeObjectURL(url)
-  }, 100)
-}
 
 const getMediaStream = (stream) => {
   let video = document.querySelector('video')
@@ -164,15 +75,6 @@ const getMediaStream = (stream) => {
     let audioTracks = microAudioStream.getAudioTracks()
     localStream.addTrack(audioTracks[0])
   }
-  // if (includeSysAudio) {
-    // console.log('Adding system audio track.')
-    // let audioTracks = stream.getoAudioTracks()
-    // if (audioTracks.length < 1) {
-      // console.log('No audio track in screen stream.')
-    // }
-  // } else {
-    // console.log('Not adding audio track.')
-  // }
   try {
     console.log('Start recording the stream.')
     recorder = new MediaRecorder(stream)
@@ -185,12 +87,6 @@ const getMediaStream = (stream) => {
   recorder.start()
   console.log('Recorder is started.')
   disableButtons()
-}
-
-const getMicroAudio = (stream) => {
-  console.log('Received audio stream.')
-  microAudioStream = stream
-  stream.onended = () => { console.log('Micro audio ended.') }
 }
 
 const getUserMediaError = () => {
